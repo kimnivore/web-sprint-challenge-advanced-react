7ad2af4236722d64a56cb9c9d18ef1c1ef255703
Kim Nguyen <kimnivore@gmail.com>
2022-01-13 19:17:25 -0800

    initial structure for useform

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,25 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+
+const useForm = (initialValue) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [value, setValue] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+
+      return ([showSuccessMessage, value, handleChanges, handleSubmit]);
+
+}
+
+export default useForm;
